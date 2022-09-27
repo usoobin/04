775970170ae46dce5897b1d5cb2f2fed8a148b5c
usoobin <112929005+usoobin@users.noreply.github.com>
2022-09-27 17:02:17 +0900

    20220927

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 	scanf("%i",&y);	
 	
 	
-	if (y%4==0 && y%100 !=0 || y%400 == 0)		
+	if ((y%4==0 && y%100 !=0 )|| y%400 == 0)		
 		printf("%i is  the leap year \n",y);
 	
 	else
