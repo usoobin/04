5571d39b326903bd4b97458a1885bc0007a15b3d
usoobin <112929005+usoobin@users.noreply.github.com>
2022-09-27 17:03:51 +0900

    20220927

--- main.c
+++ main.c
@@ -10,12 +10,9 @@ int main(int argc, char *argv[])
 	printf("input the year:");
 	scanf("%i",&y);	
 	
-	
-	if ((y%4==0 && y%100 !=0 )|| y%400 == 0)		
-		printf("%i is  the leap year \n",y);
-	
-	else
-		printf("%i is not the leap year \n",y);
+
+		
+	printf("%i : %i\n",(y%4==0 && y%100 !=0 )|| y%400 == 0);
 			
 	
 
