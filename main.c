347a701c0573319ccf408ec97d56a5744db41c8d
usoobin <112929005+usoobin@users.noreply.github.com>
2022-09-27 16:34:45 +0900

    20220927

--- main.c
+++ main.c
@@ -5,21 +5,19 @@
 
 int main(int argc, char *argv[]) 
 {
-	int x,y,z,m;
-	int a,b,c;
-	x=1;
-	y=3;
-	z=5;
-	a=3;
-	b=4;
-	c=5;
-	
-	y=a*x*x+b*x+c;
-	
-	m=(x+y+z)/3;
-	
-	printf("y=%d, m=%d",y,m);
+	int a,b;
+
+		
+	printf("input two integers :");
+	scanf("%i %i",&a,&b);	
 	
 	
+	printf("+ result is %i\n",a+b);
+	printf("- result is %i\n",a-b);
+	printf("* result is %i\n",a*b);
+	printf("/ result is %i\n",a/b);
+	printf("%% result is %i\n",a%b);
+
+
 	return 0;
 }
